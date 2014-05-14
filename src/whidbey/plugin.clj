0889ae439ee07d44e3d2255948ba253023e8c817
Greg Look <greg@greg-look.net>
2014-05-14 11:19:24 -0700

    Add leiningen plugin middleware to automatically load whidbey for repls.

---
+++ src/whidbey/plugin.clj
@@ -0,0 +1,28 @@
+(ns whidbey.plugin
+  (:require
+    [leiningen.core.project :as project]))
+
+
+(defn whidbey-profile
+  [renderer]
+  (when renderer
+    `{:dependencies
+      [[mvxcvi/puget "0.5.1"]
+       [mvxcvi/whidbey "RELEASE"]]
+
+      :repl-options
+      {:init (require 'clojure.tools.nrepl.middleware.render-values '~(symbol (namespace renderer)))
+       :nrepl-middleware [clojure.tools.nrepl.middleware.render-values/render-values]
+       :nrepl-context {:interactive-eval {:renderer ~renderer}}}}))
+
+
+(defn middleware
+  [project]
+  (let [renderer (or (:whidbey-renderer project)
+                     'puget.printer/cprint-str)
+        profile (merge (whidbey-profile renderer)
+                       (:whidbey (:profiles project)))]
+    (-> project
+        (project/add-profiles {:whidbey profile})
+        (update-in [:profiles :repl]
+                   #(if % [:whidbey %] [:whidbey])))))
