17050ea80c994d71007a34072cce98cc9e835eb8
Greg Look <greg@greg-look.net>
2014-09-23 22:41:50 -0700

    Bump version to 0.3.3.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mvxcvi/whidbey "0.3.2"
+(defproject mvxcvi/whidbey "0.3.3"
   :description "nREPL middleware to allow arbitrary value rendering."
   :url "https://github.com/greglook/whidbey"
   :license {:name "Public Domain"
