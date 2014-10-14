d46eb0adfc2234acda1e0d888e1a8f1d4bf09417
Greg Look <greg@greg-look.net>
2014-10-14 12:24:28 -0700

    Adjust indent style in project definition.

--- project.clj
+++ project.clj
@@ -4,11 +4,7 @@
   :license {:name "Public Domain"
             :url "http://unlicense.org/"}
 
-  :dependencies
-  [[mvxcvi/puget "0.6.4"]
-   [org.clojure/tools.nrepl "0.2.6"]]
+  :dependencies [[mvxcvi/puget "0.6.4"]
+                 [org.clojure/tools.nrepl "0.2.6"]]
 
-  :profiles
-  {:dev
-   {:dependencies
-    [[org.clojure/clojure "1.6.0"]]}})
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]]}})
