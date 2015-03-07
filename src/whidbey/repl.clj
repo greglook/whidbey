70fc11abcf03a2cbb904c9de6b43b9af74c53b06
Greg Look <greg@greg-look.net>
2015-03-07 15:52:26 -0800

    Simplify repl ns.

--- src/whidbey/repl.clj
+++ src/whidbey/repl.clj
@@ -1,22 +1,11 @@
 (ns whidbey.repl
   (:require
-    (clojure.tools.nrepl.middleware
-      [pr-values :refer [pr-values]]
-      [render-values :refer [render-values]])
-    [clojure.tools.nrepl.server :refer [default-handler]]
+    [clojure.tools.nrepl.middleware.pr-values :refer [pr-values]]
     [whidbey.render :as render]))
-
-
-(defn wrap-nrepl-handler!
-  "Alters the nrepl server handler by wrapping it with Whidbey's `render-values`
-  middleware and deactivating the default `pr-value`."
-  []
-  ;(alter-var-root #'default-handler #'render-values)
-  (alter-var-root #'pr-values (constantly identity)))
 
 
 (defn init!
   "Initializes the repl to use Whidbey's customizations."
   [options]
   (render/update-options! options)
-  (wrap-nrepl-handler!))
+  (alter-var-root #'pr-values (constantly identity)))
