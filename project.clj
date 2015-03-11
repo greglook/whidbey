21f355306ff361d45d589df98a0a51789d3b3bfd
Greg Look <greg@greg-look.net>
2015-03-11 00:00:12 -0700

    Set release version and Puget dependency.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mvxcvi/whidbey "0.6.0-SNAPSHOT"
+(defproject mvxcvi/whidbey "0.6.0"
   :description "nREPL middleware to allow arbitrary value rendering."
   :url "https://github.com/greglook/whidbey"
   :license {:name "Public Domain"
@@ -7,5 +7,5 @@
   :deploy-branches ["master"]
   :eval-in-leiningen true
 
-  :dependencies [[mvxcvi/puget "0.8.0-SNAPSHOT"]
+  :dependencies [[mvxcvi/puget "0.8.0"]
                  [org.clojure/data.codec "0.1.0"]])
