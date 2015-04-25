35dc22f5a467099997e0600abf716cfb3fb0a0c1
Greg Look <greg@greg-look.net>
2015-04-25 14:26:42 -0700

    Update puget to 0.8.1.

--- project.clj
+++ project.clj
@@ -7,5 +7,5 @@
   :deploy-branches ["master"]
   :eval-in-leiningen true
 
-  :dependencies [[mvxcvi/puget "0.8.0"]
+  :dependencies [[mvxcvi/puget "0.8.1"]
                  [org.clojure/data.codec "0.1.0"]])
