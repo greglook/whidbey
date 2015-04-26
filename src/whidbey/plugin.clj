4d1e457a3725e0860695447fc5a5820186fc8a0f
Greg Look <greg@greg-look.net>
2015-04-26 12:23:56 -0700

    Simplify plugin logic to merge in a profile.

--- src/whidbey/plugin.clj
+++ src/whidbey/plugin.clj
@@ -16,83 +16,33 @@
       nil)))
 
 
-(defn- plugin-dependency
-  "Looks up the version for the given plugin, or sets it to `\"RELEASE\"`.
-  Returns a dependency vector entry."
-  [project plugin]
-  (vector
-   plugin
-   (or (find-plugin-version project plugin)
-       "RELEASE")))
-
-
-(defn- add-dependencies
-  "Adds some dependencies to the end of the current vector."
-  [project & deps]
-  (update-in project [:dependencies] concat deps))
-
-
-(defn- add-repl-init
-  "Adds the given repl initialization forms to the `:init` key in
-  `:repl-options`. Any existing initialization will happen before the
-  form is executed."
-  [project & forms]
-  (update-in project
-             [:repl-options :init]
-             (fn [current]
-               (if current
-                 `(do ~current ~@forms)
-                 `(do ~@forms)))))
-
-
-(defn- add-nrepl-middleware
-  "Adds the middleware identified by the given symbol to the *front* of the
-  current nrepl-middleware list in the project map."
-  [project sym]
-  (update-in project
-             [:repl-options :nrepl-middleware]
-             #(into [sym] %)))
-
-
-(defn- set-interactive-eval-renderer
-  "Sets the nrepl renderer for the interactive-eval context to the function
-  named by the given symbol."
-  [project sym]
-  (assoc-in project
-            [:repl-options :nrepl-context :interactive-eval :renderer]
-            sym))
+(defn- whidbey-profile
+  "Constructs a profile map for enabling the repl hooks."
+  [version options]
+  (->
+    `{:dependencies [[mvxcvi/whidbey ~(or version "RELEASE")]]
+      :repl-options {:nrepl-middleware [clojure.tools.nrepl.middleware.render-values/render-values]
+                     :nrepl-context {:interactive-eval {:renderer whidbey.render/render-str}}
+                     :init (do (require 'whidbey.repl)
+                               (whidbey.repl/init! ~options))}}
+    (vary-meta assoc :repl true)))
 
 
 (defn middleware
   "Rewrites the project to include Whidbey's customizations when the project
   includes one of the target profiles."
   [project]
-  ; TODO: stop accepting :puget-options eventually.
-  (let [options (:whidbey project (:puget-options project))
-        active-profiles (:included-profiles (meta project))
-        target-profiles (:target-profiles options #{:repl})]
-    (cond
-      ; FIXME: Don't activate on standalone-repls because Puget doesn't load.
-      (nil? (:name project))
-        project
+  (cond
+    ; FIXME: Don't activate on standalone-repls because Puget doesn't load.
+    (nil? (:name project))
+      project
 
-      ; Idempotent if the project has already been updated.
-      (::included (meta project))
-        project
+    ; Idempotent if the profile already exists.
+    (:whidbey/repl (:profiles project))
+      project
 
-      ; Only modify the project if desired profiles are present.
-      (some (set target-profiles) active-profiles)
-        (-> project
-            (add-dependencies
-              (plugin-dependency project 'mvxcvi/whidbey))
-            (add-repl-init
-              `(require 'whidbey.repl)
-              `(whidbey.repl/init! ~options))
-            (add-nrepl-middleware
-              'clojure.tools.nrepl.middleware.render-values/render-values)
-            (set-interactive-eval-renderer
-              'whidbey.render/render-str)
-            (vary-meta assoc ::included true))
-
-      ; Otherwise, return project unchanged.
-      :else project)))
+    :else
+      (let [options (:whidbey project)
+            version (find-plugin-version project 'mvxcvi/whidbey)
+            profile (whidbey-profile version options)]
+        (project/add-profiles project {:whidbey/repl profile}))))
