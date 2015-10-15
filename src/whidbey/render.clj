635ddf5c7c835ec660810bcbf40cfbc3de9e10f5
Greg Look <greg@greg-look.net>
2015-10-15 10:10:30 -0700

    Keep printer record and add custom handler when rendering.

--- src/whidbey/render.clj
+++ src/whidbey/render.clj
@@ -1,16 +1,20 @@
 (ns whidbey.render
   (:require
     [puget.dispatch :as dispatch]
-    [puget.printer :as puget]))
+    [puget.printer :as puget]
+    [whidbey.types :as types]))
+
 
 ;; ## Rendering Options
 
-(def options
-  "Currently configured Puget options."
-  {:print-color true
-   :escape-types #{'clj_http.headers.HeaderMap
-                   'datomic.btset.BTSet
-                   'datomic.db.Db}})
+(def printer
+  "Currently configured Puget printer record."
+  (puget/pretty-printer
+    {:print-color true
+     :extend-notation true
+     :escape-types #{'clj_http.headers.HeaderMap
+                     'datomic.btset.BTSet
+                     'datomic.db.Db}}))
 
 
 (defn update-options!
@@ -19,48 +23,19 @@
   (alter-var-root #'options puget/merge-options opts))
 
 
-
-;; ## Type Extensions
-
-(defn bin-str
-  "Renders a byte array as a base-64 encoded string."
-  [bin]
-  (apply str (map char (b64/encode bin))))
-
-
-(defn read-bin
-  "Reads a base64-encoded string into a byte array. Suitable as a data-reader
-  for `whidbey/bin` literals."
-  ^bytes
-  [^String bin]
-  (b64/decode (.getBytes bin)))
-
-
-(defn read-uri
-  "Constructs a URI from a string value. Suitable as a data-reader for
-  `whidbey/uri` literals."
-  ^URI
-  [^String uri]
-  (URI. uri))
-
-
-(def tag-handlers
-  "Extra print-handlers for Whidbey's repl tag extensions."
-  {(class (byte-array 0)) (puget/tagged-handler 'whidbey/bin bin-str)
-   java.net.URI           (puget/tagged-handler 'whidbey/uri str)})
-
-
-(defn wrap-handlers
-  "Wraps custom handlers around an existing dispatch."
-  [dispatch]
+(def print-handlers
+  "Custom handler lookup for Whidbey's printer."
   (dispatch/chained-lookup
-    (when-let [escaped (seq (:escape-types options))]
-      (dispatch/symbolic-lookup
-        (fn lookup [t]
-          (when (some #{t} escaped)
-            puget/pr-handler))))
-    dispatch
-    tag-handlers
+    (dispatch/symbolic-lookup
+      (fn [t]
+        (when (some #{t} (:escape-types options))
+          puget/pr-handler)))
+    (fn [t]
+      (when-let [custom-lookup (:print-handlers options)]
+        (custom-lookup options)))
+    (fn [t]
+      (when (:extend-notation options)
+        (types/tag-handlers t)))
     puget/common-handlers))
 
 
@@ -71,4 +46,6 @@
   "Renders the given value to a display string by pretty-printing it using Puget
   and the configured options."
   [value]
-  (puget/pprint-str value options))
+  (-> printer
+      (assoc :print-handlers print-handlers)
+      (puget/render-str value)))
