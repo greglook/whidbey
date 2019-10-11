9f011e8b2547f66caf734eefb68046f5ede78586
Greg Look <greg@amperity.com>
2019-10-11 13:15:40 -0700

    Bump SNAPSHOT version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mvxcvi/whidbey "2.1.1"
+(defproject mvxcvi/whidbey "2.2.0-SNAPSHOT"
   :description "nREPL middleware to allow arbitrary value rendering."
   :url "https://github.com/greglook/whidbey"
   :license {:name "Public Domain"
